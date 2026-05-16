31d0f1fd0de4e9cdd3655ebce4e442f45cb3ff41
Mohamed Aziz Gadhgadhi <mohamedaziz.ghadgadhi@insat.ucar.tn>
2026-05-16 20:00:00 +0200

    fix: add password encryption

--- pages/auth/processLogin.php
+++ pages/auth/processLogin.php
@@ -6,7 +6,7 @@ $email=$_POST['email'];
 $user=$userRepository->findById($email);
 $pwd=$_POST['password'];
 $db_pwd=$user->password;
-if(isset($db_pwd) && $db_pwd==$pwd){
+if(isset($db_pwd) && (password_verify($pwd, $db_pwd))){
     $_SESSION['user']=$email;
     header('location:search.php');
 } else {
