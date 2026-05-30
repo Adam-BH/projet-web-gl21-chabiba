ba3d7dcb30719fe0963e9a096e5dd7bab9cc41a4
Akrem <akrem.medimagh10@ieee.org>
2026-05-30 02:48:38 +0100

    Fix lost and found button layout

--- public_html/lost-and-found/itemlist.php
+++ public_html/lost-and-found/itemlist.php
@@ -19,27 +19,25 @@ if (!$posts || count($posts) === 0) {
 
             <img src="<?= htmlspecialchars($photoPath) ?>" alt="Item Picture">
             <div class="post-content">
-                <div class="post-content">
-                    <div class="item">
-                        Item: <?= htmlspecialchars(strtoupper($post->item)) ?>
-                    </div>
-                    <div class="place">
-                        Found in: <?= htmlspecialchars($post->place) ?>
-                    </div>
-                    <div class="date">
-                        Posted on: <?= htmlspecialchars(strtoupper($post->created_at)) ?>
-                    </div>
-                    <div class="phone">
-                        To restore it call: <?= htmlspecialchars(strtoupper($post->phone)) ?>
-                    </div>
-                    <?php if (isset($_SESSION["is_logged"]) && $_SESSION['is_logged'] && $post->finder == $_SESSION["user"]): ?>
-                        <form action="delete_post.php" class="delete-form" method="POST"
-                            onsubmit="return confirm('Are you sure you want to delete this post? This cannot be undone.');">
-                            <input type="hidden" name="post_id" value="<?=$post->id ?>">
-                            <button type="submit" class="glowy-btn">Remove Post</button>
-                        </form>
-                    <?php endif; ?>
+                <div class="item">
+                    Item: <?= htmlspecialchars(strtoupper($post->item)) ?>
                 </div>
+                <div class="place">
+                    Found in: <?= htmlspecialchars($post->place) ?>
+                </div>
+                <div class="date">
+                    Posted on: <?= htmlspecialchars(strtoupper($post->created_at)) ?>
+                </div>
+                <div class="phone">
+                    To restore it call: <?= htmlspecialchars(strtoupper($post->phone)) ?>
+                </div>
+                <?php if (isset($_SESSION["is_logged"]) && $_SESSION['is_logged'] && $post->finder == $_SESSION["user"]): ?>
+                    <form action="delete_post.php" class="delete-form" method="POST"
+                        onsubmit="return confirm('Are you sure you want to delete this post? This cannot be undone.');">
+                        <input type="hidden" name="post_id" value="<?= $post->id ?>">
+                        <button type="submit" class="delete-btn">Remove Post</button>
+                    </form>
+                <?php endif; ?>
             </div>
         </div>
     <?php endforeach;
